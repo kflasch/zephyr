19cd388852bc9a0274475d10916a8f9d60a8ce4e
Kevin Flasch <kflasch@kflasch.net>
2020-10-04 20:59:04 -0500

    FOV updates (based on tcod v2 tutorial)

--- game_map.py
+++ game_map.py
@@ -8,9 +8,23 @@ class GameMap:
         self.width, self.height = width, height
         self.tiles = numpy.full((width, height), fill_value=tile_types.wall, order='F')
 
+        self.visible = numpy.full((width, height), fill_value=False, order='F')
+        self.explored = numpy.full((width, height), fill_value=False, order='F')
+
     def in_bounds(self, x: int, y: int) -> bool:
         """ Return true if x and y are in bounds of this map """
         return 0 <= x < self.width and 0 <= y < self.height
 
     def render(self, console: Console) -> None:
-        console.tiles_rgb[0:self.width, 0:self.height] = self.tiles['dark']
+        """ 
+        Renders the map. 
+        If a tile is in the 'visible' array, then draw it with 'light' color.
+        If it isn't, but it's in 'explored', then draw it with 'dark' color.
+        Otherwise, default is 'SHROUD'.
+        """
+        # console.tiles_rgb[0:self.width, 0:self.height] = self.tiles['dark']
+        console.tiles_rgb[0:self.width, 0:self.height] = numpy.select(
+            condlist=[self.visible, self.explored],
+            choicelist=[self.tiles["light"], self.tiles["dark"]],
+            default=tile_types.SHROUD)
+
--- tile_types.py
+++ tile_types.py
@@ -14,16 +14,24 @@ tile_dt = numpy.dtype([
     ("walkable", numpy.bool), # true if this tile can be walked through
     ("transparent", numpy.bool), # true if this tile doesn't block FOV
     ("dark", graphic_dt), # graphics for when this tile is not in FOV
+    ("light", graphic_dt), # graphics for when this tile is in FOV
 ])
 
 def new_tile(*, walkable: int, transparent: int,
-             dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]]) -> numpy.ndarray:
+             dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+             light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]]) -> numpy.ndarray:
     """ Helper fn for defining individual tile types """
-    return numpy.array((walkable, transparent, dark), dtype=tile_dt)
+    return numpy.array((walkable, transparent, dark, light), dtype=tile_dt)
 
+# represents unexplored, unseen tiles
+SHROUD = numpy.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 
 floor = new_tile(
-    walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150)))
+    walkable=True, transparent=True,
+    dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    light=(ord(" "), (255, 255, 255), (200, 180, 50)))
 
 wall = new_tile(
-    walkable=False, transparent=False, dark=(ord(" "), (255, 255, 255), (0, 0, 100)))
+    walkable=False, transparent=False,
+    dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+    light=(ord(" "), (255, 255, 255), (130, 110, 50)))
